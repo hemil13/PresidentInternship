4422242b8aa1fd1c2930ec23c163b01a6f20ddd6
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2025-07-30 13:02:32 +0530

    Profile data set

--- app/src/main/java/com/example/presidentinternship/ConstantSp.java
+++ app/src/main/java/com/example/presidentinternship/ConstantSp.java
@@ -6,4 +6,7 @@ public class ConstantSp {
 
     public static final String userid = "USERID";
     public static final String name = "NAME";
+    public static final String email = "EMAIL";
+    public static final String contact = "CONTACT";
+    public static final String password = "PASSWORD";
 }
--- app/src/main/java/com/example/presidentinternship/ProfileActivity.java
+++ app/src/main/java/com/example/presidentinternship/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.presidentinternship;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,20 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    EditText cnfpassword;
+    EditText cnfpassword, name, email, contact, password;
     Button edit, update;
+
+    SharedPreferences sp;
+
+    String email_pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        sp = getSharedPreferences(ConstantSp.pref, MODE_PRIVATE);
+
         cnfpassword = findViewById(R.id.profile_cnfpassword);
         edit = findViewById(R.id.profile_edit);
         update = findViewById(R.id.profile_update);
-
-
-
+        name = findViewById(R.id.profile_name);
+        email = findViewById(R.id.profile_email);
+        contact = findViewById(R.id.profile_contact);
+        password = findViewById(R.id.profile_password);
+    
+        setData(false);
 
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,17 +44,78 @@ public class ProfileActivity extends AppCompatActivity {
                 update.setVisibility(View.VISIBLE);
                 edit.setVisibility(View.GONE);
                 cnfpassword.setVisibility(View.VISIBLE);
+
+                setData(true);
             }
         });
 
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                update.setVisibility(View.GONE);
-                edit.setVisibility(View.VISIBLE);
-                cnfpassword.setVisibility(View.GONE);
+
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Enter Email");
+                } else if (!email.getText().toString().matches(email_pattern)) {
+                    email.setError("Enter a Valid Email");
+                }
+
+                else if(name.getText().toString().trim().equals("")){
+                    name.setError("Enter Name");
+                }
+
+                else if (contact.getText().toString().trim().equals("")) {
+                    contact.setError("Enter Contact");
+                } else if (contact.getText().toString().length()<10) {
+                    contact.setError("Enter A Valid Contact");
+
+                } else if (password.getText().toString().trim().equals("")) {
+                    password.setError("Enter Password");
+                } else if (password.getText().toString().length()<6) {
+                    password.setError("Minimum 6 Characters");
+                }
+
+
+                else if (cnfpassword.getText().toString().trim().equals("")) {
+                    cnfpassword.setError("Enter Confirm Password");
+                } else if (!cnfpassword.getText().toString().matches(password.getText().toString().trim())) {
+                    cnfpassword.setError("Confirm Password Doesn't Match");
+                }
+
+                else{
+
+
+                    //
+                    sp.edit().putString(ConstantSp.name, name.getText().toString()).commit();
+                    sp.edit().putString(ConstantSp.email, email.getText().toString()).commit();
+                    sp.edit().putString(ConstantSp.contact, contact.getText().toString()).commit();
+                    sp.edit().putString(ConstantSp.password, password.getText().toString()).commit();
+
+                    //
+
+                    update.setVisibility(View.GONE);
+                    edit.setVisibility(View.VISIBLE);
+                    cnfpassword.setVisibility(View.GONE);
+
+                    setData(false);
+                }
             }
         });
 
     }
+
+    private void setData(boolean b) {
+
+        name.setText(sp.getString(ConstantSp.name, ""));
+        email.setText(sp.getString(ConstantSp.email, ""));
+        contact.setText(sp.getString(ConstantSp.contact, ""));
+        password.setText(sp.getString(ConstantSp.password, ""));
+
+
+        name.setEnabled(b);
+        email.setEnabled(b);
+        contact.setEnabled(b);
+        password.setEnabled(b);
+    }
+
+
 }
